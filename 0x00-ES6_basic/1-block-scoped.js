9ca52da1bcc6e1bba400e08a1201a392356d381e
reunicorn1 <138371895+reunicorn1@users.noreply.github.com>
2024-05-27 22:57:41 +0400

    return true or false directly without redefining the variables in the if block

--- 0x00-ES6_basic/1-block-scoped.js
+++ 0x00-ES6_basic/1-block-scoped.js
@@ -3,9 +3,7 @@ export default function taskBlock(trueOrFalse) {
   const task2 = true;
 
   if (trueOrFalse) {
-    const task2 = true;
-    const task = false;
-    return [task, task2]
+    return [false, true];
   }
 
   return [task, task2];
