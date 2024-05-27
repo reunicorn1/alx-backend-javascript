f66ff4c00180880373eb45128b6b08e211c10034
reunicorn1 <138371895+reunicorn1@users.noreply.github.com>
2024-05-27 22:52:53 +0400

    manipulate contents of the block since they don't affect the global variables

--- 0x00-ES6_basic/1-block-scoped.js
+++ 0x00-ES6_basic/1-block-scoped.js
@@ -3,8 +3,10 @@ export default function taskBlock(trueOrFalse) {
   const task2 = true;
 
   if (trueOrFalse) {
-    const task = true;
-    const task2 = false;
+    let task = true;
+    let task2 = false;
+    task = task2;
+    task2 = task;
   }
 
   return [task, task2];
