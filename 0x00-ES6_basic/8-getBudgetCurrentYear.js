5e0af572f28f71b9c1af67499e7bc974f8752cd2
reunicorn1 <138371895+reunicorn1@users.noreply.github.com>
2024-05-28 08:42:13 +0400

    adding a comma at the end of the file

--- 0x00-ES6_basic/8-getBudgetCurrentYear.js
+++ 0x00-ES6_basic/8-getBudgetCurrentYear.js
@@ -5,10 +5,11 @@ function getCurrentYear () {
 
 export default function getBudgetForCurrentYear(income, gdp, capita) {
   const year = getCurrentYear();
+
   const budget = {
     [`income-${year}`]: income,
     [`gdp-${year}`]: gdp,
-    [`capita-${year}`]: capita
+    [`capita-${year}`]: capita,
   };
 
   return budget;
