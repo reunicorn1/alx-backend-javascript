7f18bf1f224175c275b9af740468a95cee6eb5d9
reunicorn1 <138371895+reunicorn1@users.noreply.github.com>
2024-05-29 18:46:47 +0400

    6-final-user.js: modifying the array to have a value parameter in place of a reason

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -2,10 +2,12 @@ import signUpUser from './4-user-promise';
 import uploadPhoto from './5-photo-reject';
 
 export default function handleProfileSignup(firstName, lastName, fileName) {
-  const promises = [signUpUser(firstName, lastName), uploadPhoto(fileName)];
-  return Promise.allSettled(promises)
-    .then((results) => results.map((result) => ({
+  const signUpPromise = signUpUser(firstName, lastName);
+  const uploadPromise = uploadPhoto(fileName);
+
+  return Promise.allSettled([signUpPromise, uploadPromise])
+    .then((results) => (results.map((result) => ({
       status: result.status,
-      value: result.status === 'rejected' ? result.reason : result.value,
-    })));
+      value: result.status === 'fulfilled' ? result.value : result.reason,
+    }))));
 }
