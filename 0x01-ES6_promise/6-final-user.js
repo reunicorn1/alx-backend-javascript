c6f0ac0c4b4afc7834c190a7a4344dac7b38236b
reunicorn1 <138371895+reunicorn1@users.noreply.github.com>
2024-05-29 18:28:54 +0400

    6-final-user.js

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -1,7 +1,6 @@
 import signUpUser from './4-user-promise';
 import uploadPhoto from './5-photo-reject';
 
-export default async function handleProfileSignup(firstName, lastName, fileName) {
-  const result = await Promise.allSettled([uploadPhoto(fileName), signUpUser(firstName, lastName)]);
-  return result;
+export default function handleProfileSignup(firstName, lastName, fileName) {
+  return Promise.allSettled([uploadPhoto(fileName), signUpUser(firstName, lastName)]);
 }
