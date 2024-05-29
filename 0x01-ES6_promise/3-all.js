248d6bc6dd027ed9ec907869b8258bfb5b36b534
reunicorn1 <138371895+reunicorn1@users.noreply.github.com>
2024-05-29 17:43:43 +0400

    3-all.js: changing it to a promise.all

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -2,8 +2,7 @@ import { uploadPhoto, createUser } from './utils';
 
 export default async function handleProfileSignup() {
   try {
-    const response = await uploadPhoto();
-    const user = await createUser();
+    const [response, user] = await Promise.all([uploadPhoto(), createUser()]);
     console.log(`${response.body} ${user.firstName} ${user.lastName}`);
   } catch (err) {
     console.log('Signup system offline');
