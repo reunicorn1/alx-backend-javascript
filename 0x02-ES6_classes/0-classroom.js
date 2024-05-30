7898d65ea1f50bec9795d2cb8f3e6d43337bcf9e
reunicorn1 <138371895+reunicorn1@users.noreply.github.com>
2024-05-30 22:23:27 +0400

    0-classroom.js

---
+++ 0x02-ES6_classes/0-classroom.js
@@ -0,0 +1,12 @@
+/**
+ * Represents a ClassRoom.
+ */
+export default class ClassRoom {
+  /**
+   *
+   * @param {number} maxStudentsSize - maximum number of students.
+   */
+  constructor(maxStudentsSize) {
+    this._maxStudentsSize = maxStudentsSize;
+  }
+}
