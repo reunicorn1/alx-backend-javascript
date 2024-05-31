814776272e047982f44180e771f17ac0f9d87dd4
reunicorn1 <138371895+reunicorn1@users.noreply.github.com>
2024-05-31 05:53:19 +0400

    100-evcar.js - adding getters and setters for range

--- 0x02-ES6_classes/100-evcar.js
+++ 0x02-ES6_classes/100-evcar.js
@@ -2,11 +2,16 @@ import Car from './10-car';
 
 export default class EVCar extends Car {
   constructor(brand, motor, color, range) {
-    super();
-    this._brand = brand;
-    this._motor = motor;
-    this._color = color;
+    super(brand, motor, color);
     this._range = range;
+  }
+
+  get range() {
+    return this._range;
+  }
+
+  set range(value) {
+    this._range = value;
   }
 
   cloneCar() {
