6a75a708a4dbabcc34bc62da71b7eb1feeff6ab1
reunicorn1 <138371895+reunicorn1@users.noreply.github.com>
2024-06-04 13:30:07 +0400

    8-clean_set.js - checking the valur of the item as string

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -2,7 +2,7 @@ export default function cleanSet(set, startString) {
   if (set && set instanceof Set && startString && typeof startString === 'string') {
     const concat = [];
     set.forEach((item) => {
-      if (item.startsWith(startString)) {
+      if (typeof item === 'string' && item.startsWith(startString)) {
         concat.push(item.slice(startString.length));
       }
     });
