c4b140b8b0696c8c44dd224a9c51593f3618cfb4
reunicorn1 <138371895+reunicorn1@users.noreply.github.com>
2024-06-04 12:09:49 +0400

    8-clean_set.js - checked for the type of the string argument

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -1,5 +1,5 @@
 export default function cleanSet(set, startString) {
-  if (set instanceof Set && startString) {
+  if (set instanceof Set && typeof startString === 'string' && startString) {
     const concat = [];
     set.forEach((item) => {
       if (item.startsWith(startString)) {
