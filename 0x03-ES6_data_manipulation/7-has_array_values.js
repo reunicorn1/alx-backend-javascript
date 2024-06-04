e1e752d62e645a51d16fd2f7e6c0a30b06c9b0e8
reunicorn1 <138371895+reunicorn1@users.noreply.github.com>
2024-06-04 11:46:40 +0400

    7-has_array_values.js - using the method has of set rather than converting the set to an array

--- 0x03-ES6_data_manipulation/7-has_array_values.js
+++ 0x03-ES6_data_manipulation/7-has_array_values.js
@@ -1,6 +1,7 @@
 export default function hasValuesFromArray(set, array) {
   if (set instanceof Set && Array.isArray(array)) {
-    return array.every((item) => [...set].includes(item));
+    return array.every((item) => set.has(item));
+    // return array.every((item) => [...set].includes(item));
   }
   return undefined;
 }
