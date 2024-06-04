5518e35abb554d1602e7d90a27a9b323cf1405de
reunicorn1 <138371895+reunicorn1@users.noreply.github.com>
2024-06-04 13:28:33 +0400

    8-clean_set.js - literally i have no idea

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -1,8 +1,5 @@
 export default function cleanSet(set, startString) {
-  if (!(set instanceof Set || Array.from(set).every((item) => typeof item === 'string'))) {
-    return undefined;
-  }
-  if (typeof startString === 'string' && startString) {
+  if (set && set instanceof Set && startString && typeof startString === 'string') {
     const concat = [];
     set.forEach((item) => {
       if (item.startsWith(startString)) {
