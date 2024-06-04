a29c47c0c9ac163944122e8beeb2c084d6f55a3a
reunicorn1 <138371895+reunicorn1@users.noreply.github.com>
2024-06-04 12:34:02 +0400

    8-clean_set.js - when set is not valid return undefined

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -1,5 +1,8 @@
 export default function cleanSet(set, startString) {
-  if (set instanceof Set && Array.from(set).every((item) => typeof item === 'string') && typeof startString === 'string' && startString) {
+  if (!(set instanceof Set && Array.from(set).every((item) => typeof item === 'string'))) {
+    return undefined;
+  }
+  if (typeof startString === 'string' && startString) {
     const concat = [];
     set.forEach((item) => {
       if (item.startsWith(startString)) {
