29fedc76b382e383f5c3ce27ade68f00418c3cb5
reunicorn1 <138371895+reunicorn1@users.noreply.github.com>
2024-06-06 02:54:49 +0400

    4. Writing a class

--- 0x04-TypeScript/task_1/js/main.ts
+++ 0x04-TypeScript/task_1/js/main.ts
@@ -1,3 +1,4 @@
+// 1. Let's build a Teacher interface
 interface Teacher {
 	readonly firstName: string,
 	readonly lastName: string,
@@ -5,19 +6,6 @@ interface Teacher {
 	yearsOfExperience?: number,
 	location: string,
 	[key: string]: any
-}
-
-interface Directors extends Teacher {
-	numberOfReports: number,
-}
-
-interface printTeacherFunction {
-	(param1: string, param2: string): string;
-}
-
-let printTeacher: printTeacherFunction;
-printTeacher = function(firstName: string, lastName: string): string {
-	return `${firstName[0]}. ${lastName}`;
 }
 
 const teacher3: Teacher = {
@@ -31,6 +19,12 @@ const teacher3: Teacher = {
 
 console.log(teacher3);
 
+
+// 2. Extending the Teacher class
+interface Directors extends Teacher {
+	numberOfReports: number,
+}
+
 const director1: Directors = {
   firstName: 'John',
   lastName: 'Doe',
@@ -41,4 +35,51 @@ const director1: Directors = {
 
 console.log(director1);
 
+
+// 3. Printing teachers
+interface printTeacherFunction {
+	(param1: string, param2: string): string;
+}
+
+let printTeacher: printTeacherFunction;
+printTeacher = function(firstName: string, lastName: string): string {
+	return `${firstName[0]}. ${lastName}`;
+}
 console.log(printTeacher("John", "Doe"));
+
+
+// 4. Writing a class
+interface Student {
+	firstName: string;
+	lastName: string;
+	workOnHomework(): string;
+	displayName(): string
+}
+
+interface StudentConstructor {
+  new (firstName: string, lastName: string): Student;
+}
+
+
+class StudentClass implements Student {
+	firstName: string;
+	lastName: string;
+
+	constructor(firstName: string, lastName: string) {
+		this.firstName = firstName;
+		this.lastName = lastName;
+	}
+	workOnHomework(): string {
+		return 'Currently working';
+	}
+	displayName(): string {
+		return this.firstName;
+	}
+}
+
+
+const studentFactory: StudentConstructor = StudentClass;
+
+const student: Student = new studentFactory('John', 'Doe');
+console.log(student.displayName());
+console.log(student.workOnHomework());
