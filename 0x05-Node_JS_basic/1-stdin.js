e496a387485b86933d5aa9e01ea874de9c687a98
reunicorn1 <138371895+reunicorn1@users.noreply.github.com>
2024-07-23 23:29:16 +0400

    using process stdin using process.write

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -1,4 +1,4 @@
-console.log('Welcome to Holberton School, what is your name?');
+process.stdout.write('Welcome to Holberton School, what is your name?\n');
 
 process.stdin.on('data', (data) => {
   if (data) {
@@ -7,5 +7,5 @@ process.stdin.on('data', (data) => {
 });
 
 process.on('exit', () => {
-  console.log('This important software is now closing');
+  process.stdout.write('This important software is now closing\n');
 });
