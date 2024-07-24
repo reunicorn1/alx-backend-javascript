2120d047b19f294cbff88815b96d9e7ff8cf145c
reunicorn1 <138371895+reunicorn1@users.noreply.github.com>
2024-07-24 13:34:54 +0400

    4-http.js redo2

--- 0x05-Node_JS_basic/4-http.js
+++ 0x05-Node_JS_basic/4-http.js
@@ -3,7 +3,6 @@ const http = require('http');
 const PORT = 1245;
 
 const app = http.createServer((req, res) => {
-  res.writeHead(200, { 'Content-Type': 'text/plain' });
   res.end('Hello Holberton School!');
 });
 
