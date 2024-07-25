7feb8ee02f21d34a44cf2e91f2bcf4ddce1fa676
reunicorn1 <138371895+reunicorn1@users.noreply.github.com>
2024-07-25 22:38:51 +0400

    3-payment.js

---
+++ 0x06-unittests_in_js/3-payment.test.js
@@ -0,0 +1,15 @@
+const sendPaymentRequestToApi = require('./3-payment');
+const Utils = require('./utils');
+const chai = require('chai');
+const expect = chai.expect;
+const sinon = require("sinon");
+
+utility = new Utils()
+
+describe("sendPaymentRequestToApi", () => {
+    it("spy the utils calculateNumber method", () => {
+        const spy = sinon.spy(utility, "calculateNumber");
+        sendPaymentRequestToApi(100, 20);
+        expect(spy.calledOnceWith("SUM", 100, 20)).to.be.true
+    })
+})
