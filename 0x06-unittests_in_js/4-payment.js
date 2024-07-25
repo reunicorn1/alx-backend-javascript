d2bc33ff3638af07bb1cf8720627279bc13d2034
reunicorn1 <138371895+reunicorn1@users.noreply.github.com>
2024-07-25 23:33:41 +0400

    changing the payment method touse the class directly

--- 0x06-unittests_in_js/4-payment.js
+++ 0x06-unittests_in_js/4-payment.js
@@ -1,10 +1,8 @@
-const Utils = require("./utils");
-utility = new Utils()
+const Utils = require('./utils');
 
-
-function sendPaymentRequestToApi(totalAmount, totalShipping) {
-    result = utility.calculateNumber('SUM', totalAmount, totalShipping);
-    console.log(`The total is: ${result}`);
-}
+const sendPaymentRequestToApi = (totalAmount, totalShipping) => {
+  const totalCost = Utils.calculateNumber('SUM', totalAmount, totalShipping);
+  console.log(`The total is: ${totalCost}`);
+};
 
 module.exports = sendPaymentRequestToApi;
