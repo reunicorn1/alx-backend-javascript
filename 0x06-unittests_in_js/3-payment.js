151c57e250d86b329132a9f8a403a2ac89caca7a
reunicorn1 <138371895+reunicorn1@users.noreply.github.com>
2024-07-25 23:41:28 +0400

    idk

--- 0x06-unittests_in_js/3-payment.js
+++ 0x06-unittests_in_js/3-payment.js
@@ -1,8 +1,10 @@
-const Utils = require('./utils');
+const Utils = require("./utils");
+utility = new Utils()
 
-const sendPaymentRequestToApi = (totalAmount, totalShipping) => {
-  const totalCost = Utils.calculateNumber('SUM', totalAmount, totalShipping);
-  console.log(`The total is: ${totalCost}`);
-};
+
+function sendPaymentRequestToApi(totalAmount, totalShipping) {
+    result = utility.calculateNumber('SUM', totalAmount, totalShipping);
+    console.log(`The total is: ${result}`);
+}
 
 module.exports = sendPaymentRequestToApi;
